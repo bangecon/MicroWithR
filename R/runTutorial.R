a8201136be1b19001dac0b0ed487c350d3b0f163
James Bang <bangjamest@sau.edu>
2023-03-20 15:35:58 -0500

    run

--- R/runTutorial.R
+++ R/runTutorial.R
@@ -1,11 +1,20 @@
 #' @export
-runTutorial <- function(module, lesson) {
-  tutorial <- paste0("tutorials/", module, "_", lesson)
-  appDir <- system.file(tutorial, package = "MicroWithR")
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `microWithR`.", call. = FALSE)
+runTutorial <- function(module, lesson, practice = FALSE) {
+  if(practice == TRUE) {
+    tutorial <- paste0("tutorials/", module, "_", lesson, "_Practice")
+    appDir <- system.file(tutorial, package = "MicroWithR")
+    if (appDir == "") {
+      stop("Could not find example directory. Try re-installing `MicroWithR`.", call. = FALSE)
+    }
+    file = paste0(appDir, "/", module, "_", lesson, "_Practice.Rmd")
+  } else {
+    tutorial <- paste0("tutorials/", module, "_", lesson)
+    appDir <- system.file(tutorial, package = "MicroWithR")
+    if (appDir == "") {
+      stop("Could not find example directory. Try re-installing `MicroWithR`.", call. = FALSE)
+    }
+    file = paste0(appDir, "/", module, "_", lesson, ".Rmd")
   }
-  file = paste0(appDir, "/", chapter, "_", lesson, ".Rmd")
 
 
   learnr::run_tutorial(appDir)
