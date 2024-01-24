21098368c8c1701387726e7b0176faa5e6cc09ad
James Bang <bangjamest@sau.edu>
2024-01-24 17:19:17 -0600

    Update Random Presenter app.R

--- inst/shiny-examples/randomPresenter/app.R
+++ inst/shiny-examples/randomPresenter/app.R
@@ -7,23 +7,30 @@ ui <- fluidPage(
   sidebarPanel(
     numericInput(
       inputId = "seed",
-      label = "Enter today's date.",
+      label = "Enter the current time (HHMMSS).",
       value = as.integer(gsub("-", "", Sys.Date()))
     ),
     textInput(
       inputId = "sheet",
       label = "Enter the sheet ID of the roster",
-      value = '1i_hJiSk-TOfqtNOtm7ZUUcJe1gEZSPwowEd5VZlHFNo'
+      value = NULL
     ),
     hr(),
     a("Created by Jim Bang", href='https://github.com/bangecon'),
     a("St. Ambrose University", href='https://www.sau.edu/')
   ),
+    actionButton("go", "Go!"),
+    numericInput(
+      inputId = "round",
+      label = "Enter the round you want to display.",
+      value = 1
+    )
+  ),
   mainPanel(textOutput("winner"), style = "font-size:40px; ")
 )
 
 server <- function(input, output) {
-  roster <- reactive({
+  roster <- eventReactive(input$go {
     sheet <- input$sheet
     g <- as.data.frame(read_sheet(sheet))
     g[,1]
